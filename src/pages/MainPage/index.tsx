92f6856cb7d8f3bfb6dd1581dc49f8de493922e3
Ilya291291 <100027113+Ilya291291@users.noreply.github.com>
2025-06-07 12:12:49 +0000

    changes

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './index.scss';
-import characters from './fixtures/characters.json';
+import characters from './fixtures/characters.json'
 
 export default function MainPage() {
   const [data, setData] = useState(null);
